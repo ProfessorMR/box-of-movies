a786178bfc8a4d071e51ceec9abca3ac8f9ee166
professor_mr <professormr29@gmail.com>
2024-11-07 15:09:45 +0330

    init project

---
+++ src/app/layout.jsx
@@ -0,0 +1,39 @@
+import { Vazirmatn } from "next/font/google";
+import "../styles/globals.css";
+
+const vazirMatn = Vazirmatn({
+  subsets: ["arabic"],
+  display: "swap",
+});
+
+export const metadata = {
+  title: "مووی باکس",
+  description: "مشاهده فیلم و سریال های دنیا به همراه نظرات کاربران",
+  icons: [
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon/favicon-16x16.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "/favicon/apple-touch-icon.png",
+    },
+  ],
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fa_IR" dir="rtl">
+      <body className={vazirMatn.className}>{children}</body>
+    </html>
+  );
+}
